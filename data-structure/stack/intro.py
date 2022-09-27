871402d57793316046fca084332be3493962507c
Sandeep Sala <sandeepsala5596@gmail.com>
2022-09-27 20:25:22 +0530

    27-09-22

---
+++ data-structure/stack/intro.py
@@ -0,0 +1,30 @@
+# Stack
+# It is a linear data structure that follows a particular order in which the operations are performed.
+
+# LIFO( Last In First Out ):
+
+# This strategy states that the element that is inserted last will come out first. You can take a pile of plates kept on top of each other as a real-life example. The plate which we put last is on the top and since we remove the plate that is at the top, we can say that the plate that was put last comes out first. 
+
+# Basic Operations on Stack
+# In order to make manipulations in a stack, there are certain operations provided to us.
+
+# push() to insert an element into the stack
+# pop() to remove an element from the stack
+# top() Returns the top element of the stack.
+# isEmpty() returns true is stack is empty else false
+# size() returns the size of stack
+
+# Time Complexity
+# Operations  	Complexity
+# push() 	O(1)
+# pop()   	O(1)
+# isEmpty() 	O(1)
+# size()	O(1)
+
+
+# Types of Stacks:
+#     Register Stack:
+#         This type of stack is also a memory element present in the memory unit and can handle a small amount of data only. The height of the register stack is always limited as the size of the register stack is very small compared to the memory.
+#     Memory Stack:
+#         This type of stack can handle a large amount of memory data. The height of the memory stack is flexible as it occupies a large amount of memory data. 
+
